c5c0a4c18989cb75fd27dd00fe644cbfa19a669a
Malthael134 <127251883+Malthael134@users.noreply.github.com>
2024-01-06 23:59:19 +0100

    SEO

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,21 @@
+<script lang="ts">
+	import { base } from "$app/paths";
+	import logo from "$lib/assets/logo/small.png";
+</script>
+
 <svelte:head>
 	<title>KWR Hannover - Informatik AG</title>
+	<meta property="og:site_name" content="KWR-Informatik-AG">
+	<meta property="og:locale" content="de">
+	<meta property="og:url" content="{base}">
+	<meta property="og:type" content="homepage">
+	<meta property="og:title" content="Startseite">
+	<meta property="og:description" content="Offizielle Hauptseite der Informatik AG am Kaiser-Wilhelm- und Ratsgymnasium Hannover.">
+	
+	<meta property="og:image" content={logo}>
+	<meta property="og:image:alt" content="Logo">
+	<meta property="og:image:width" content="100">
+	<meta property="og:image:height" content="100">
 </svelte:head>
 
 <div>
