dd691d9280be612f6a89ca333e686756da27dd14
Johann <127251883+Malthael134@users.noreply.github.com>
2024-01-12 10:22:19 +0000

    Updates

--- _app/immutable/chunks/paths.0ccc3b64.js
+++
@@ -1,1 +0,0 @@
-var s;const t=((s=globalThis.__sveltekit_1k8u6xd)==null?void 0:s.base)??"/sveltekit-github-pages";var e;const a=((e=globalThis.__sveltekit_1k8u6xd)==null?void 0:e.assets)??t;export{a,t as b};
